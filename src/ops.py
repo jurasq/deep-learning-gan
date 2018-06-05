7b7cff954eec90d3c2257950d50cd1cbed2caa5a
Daníel Freyr Hjartarson <danielf@VULCAN.DOM>
2018-06-05 22:36:15 +0200

    Added previous layer definitions from double GAN and started adapting TripleGAN structure

--- src/ops.py
+++ src/ops.py
@@ -204,7 +204,6 @@ def conv_concat(x, y):
 
     return concat([x, y * tf.ones([x_shapes[0], x_shapes[1], x_shapes[2], y_shapes[3]])], axis=3)
 
-
 def concat(x, axis=1):
     return tf.concat(x, axis=axis)
 
