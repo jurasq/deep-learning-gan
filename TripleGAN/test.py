36f59027c8425170d8d87eca12fa14c0286252c5
Daníel Freyr Hjartarson <danielf@VULCAN.DOM>
2018-06-05 11:03:53 +0200

    Interfaced code to encoded data

--- TripleGAN/test.py
+++ TripleGAN/test.py
@@ -4,4 +4,4 @@ from collections import defaultdict
 import dna
 
 if __name__ == '__main__':
-    (x_train, y_train), (x_test, y_test) = dna.load_dna_data(100, 400, "Data", ["Human", "Pig", "Dolphin"], 1)
+    (x_train, y_train), (x_test, y_test) = dna.load_dna_data(100, 400, "../Data", ["Human", "Pig", "Dolphin"], 1)
