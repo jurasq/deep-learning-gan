28eeffd99a528ebf60f0e69da4433ebb7e2df722
Daníel Freyr Hjartarson <danielf@hedinn.is>
2018-06-06 21:50:15 +0200

    Discriminator structure changed

--- src/TripleGAN.py
+++ src/TripleGAN.py
@@ -85,7 +85,24 @@ class TripleGAN(object) :
 
     def discriminator(self, x, y_, scope='discriminator', is_training=True, reuse=False):
         with tf.variable_scope(scope, reuse=reuse) :
-            x = dropout_original(x, rate=0.2, is_training=is_training)
+            x = conv_layer(name_scope="convolutional_1", input_tensor=x, num_kernels=20, kernel_shape=[4, 9], relu=True)
+            x = max_pool_layer(name_scope="max_pool_1", input_tensor=x, pool_size=[1, 3])
+
+            x = conv_layer(name_scope="convolutional_2", input_tensor=x, num_kernels=30, kernel_shape=[1, 5])
+            x = max_pool_layer(name_scope="max_pool_2", input_tensor=x, pool_size=[1, 4])
+
+            x = conv_layer(name_scope="convolutional_3", input_tensor=x, num_kernels=40, kernel_shape=[1, 3])
+            x = max_pool_layer(name_scope="max_pool_3", input_tensor=x, pool_size=[1, 4])
+
+            x = flatten(x)
+
+            x = tf.layers.dense(inputs=x, units=90)
+            x = tf.layers.dense(inputs=x, units=45)
+
+            x = tf.layers.dense(inputs=x, units=2)
+            return x
+
+            """x = dropout_original(x, rate=0.2, is_training=is_training)
             y = tf.reshape(y_, [-1, 1, 1, self.y_dim])
             x = conv_concat(x,y)
 
@@ -114,12 +131,73 @@ class TripleGAN(object) :
             out = sigmoid(x_logit)
 
 
-            return out, x_logit, x
+            return out, x_logit, x"""
 
     def generator(self, z, y, scope='generator', is_training=True, reuse=False):
         with tf.variable_scope(scope, reuse=reuse) :
+            g_dim = 64 #Number of filters of first layer of generator
+            s = self.output_height #Final length of the sequence
 
-            x = concat([z, y]) # mlp_concat
+            #We want to slowly upscale the sequence, so these values should help
+            # to make that change gradual
+            s2, s4, s8, s16 = int(s/2), int(s/4), int(s/8), int(s/16)
+
+            width = self.output_width #because we have 4 letters: ATCG
+            magic_number = 5 #this is a magic number which I'm not sure what means yet
+
+            h0 = tf.reshape(z, [batch_size, int(width/4), s16+1, magic_number])
+            h0 = tf.nn.relu(h0)
+            #Dimensions of h0 = batch_size x 1 x 31 x magic_number
+
+            #First DeConv Layer
+            output1_shape = [batch_size, int(width/2), s8+1, g_dim*4]
+            W_conv1 = tf.get_variable('g_wconv1', [5, 5, output1_shape[-1], int(h0.get_shape()[-1])],
+                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
+            b_conv1 = tf.get_variable('g_bconv1', [output1_shape[-1]], initializer=tf.constant_initializer(.1))
+            H_conv1 = tf.nn.conv2d_transpose(value=h0, filter=W_conv1, output_shape=output1_shape,
+                                             strides=[1, 2, 2, 1], padding='SAME', name="H_conv1") + b_conv1
+            H_conv1 = tf.contrib.layers.batch_norm(inputs = H_conv1, center=True, scale=True, is_training=True,
+                                                   scope="g_bn1")
+            H_conv1 = tf.nn.relu(H_conv1)
+            #Dimensions of H_conv1 = batch_size x 1 x 62 x 256
+
+
+
+            #Second DeConv Layer
+            output2_shape = [batch_size, int(width/2), s4, g_dim*2]
+            W_conv2 = tf.get_variable('g_wconv2', [5, 5, output2_shape[-1], int(H_conv1.get_shape()[-1])],
+                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
+            b_conv2 = tf.get_variable('g_bconv2', [output2_shape[-1]], initializer=tf.constant_initializer(.1))
+            H_conv2 = tf.nn.conv2d_transpose(H_conv1, W_conv2, output_shape=output2_shape,
+                                             strides=[1, 1, 2, 1], padding='SAME') + b_conv2
+            H_conv2 = tf.contrib.layers.batch_norm(inputs = H_conv2, center=True, scale=True, is_training=True,
+                                                   scope="g_bn2")
+            H_conv2 = tf.nn.relu(H_conv2)
+            #Dimensions of H_conv2 = batch_size x 2 x 124 x 128
+
+
+            #Third DeConv Layer
+            output3_shape = [batch_size, width, s2, g_dim*1]
+            W_conv3 = tf.get_variable('g_wconv3', [5, 5, output3_shape[-1], int(H_conv2.get_shape()[-1])],
+                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
+            b_conv3 = tf.get_variable('g_bconv3', [output3_shape[-1]], initializer=tf.constant_initializer(.1))
+            H_conv3 = tf.nn.conv2d_transpose(H_conv2, W_conv3, output_shape=output3_shape,
+                                             strides=[1, 2, 2, 1], padding='SAME') + b_conv3
+            H_conv3 = tf.contrib.layers.batch_norm(inputs = H_conv3, center=True, scale=True, is_training=True,
+                                                   scope="g_bn3")
+            H_conv3 = tf.nn.relu(H_conv3)
+            #Dimensions of H_conv3 = batch_size x 4 x 248 x 64
+
+
+            #Fourth DeConv Layer
+            output4_shape = [batch_size, width, s, self.c_dim]
+            W_conv4 = tf.get_variable('g_wconv4', [1, 2, output4_shape[-1], int(H_conv3.get_shape()[-1])],
+                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
+            b_conv4 = tf.get_variable('g_bconv4', [output4_shape[-1]], initializer=tf.constant_initializer(.1))
+            H_conv4 = tf.nn.conv2d_transpose(H_conv3, W_conv4, output_shape=output4_shape,
+                                             strides=[1, 1, 2, 1], padding='VALID') + b_conv4
+            H_conv4 = tf.nn.tanh(H_conv4)
+            """x = concat([z, y]) # mlp_concat
 
             x = relu(linear(x, unit=512*4*4, layer_name=scope+'_linear1'))
             x = batch_norm(x, is_training=is_training, scope=scope+'_batch1')
@@ -138,7 +216,7 @@ class TripleGAN(object) :
 
             x = tanh(deconv_layer(x, filter_size=3, kernel=[5,5], stride=2, wn=False, layer_name=scope+'deconv3'))
 
-            return x
+            return x"""
 
     def classifier(self, x, scope='classifier', is_training=True, reuse=False):
         with tf.variable_scope(scope, reuse=reuse) :
