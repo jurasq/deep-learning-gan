6434de99bf7513557b2337aa79b74430ed3c71f1
barentroia <barentroia@gmail.com>
2018-06-12 11:18:19 +0200

    levenschstein dist evaluate seqs

--- src/evaluateSequences.py
+++ src/evaluateSequences.py
@@ -11,7 +11,7 @@ import pandas as pd
 import numpy as np
 
 #used this variables for testing. will be removed.
-sequences = load_dna_data(13000,1000,"../Data",["Human"],1)
+sequences = dna.load_dna_data(13000,1000,"../Data",["Human"],1)
 train_sequences = sequences[0][0]
 test_sequences = sequences[1][0]
 
@@ -30,17 +30,37 @@ def decode_dna_matrices(sequences):
     return (decoded_sequences)
 
 
-##Computing edit distances - this is a very simple distance only considers substitutions to transform one sequence into the other
-def edit_dist(seq1,seq2):
-    return(sum([1 for s1,s2 in zip(seq1,seq2) if s1!=s2]))
+##Computing edit distances 
+#from https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#Python
+def edit_dist(s1,s2):
+    if len(s1) < len(s2):
+        return edit_dist(s2, s1)
+    # len(s1) >= len(s2)
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + 1       # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+    
+    return previous_row[-1]
     
 def pairwise_edit_distances (train_sequences,test_sequences):
+    print ("Decoding seq 1")
     dec_tr_seq = decode_dna_matrices(train_sequences)
+    print ("Decoding seq 2")
     dec_tst_seq = decode_dna_matrices(test_sequences)
     nr_tr= train_sequences.shape[0]
     nr_tst=test_sequences.shape[0]
-    edit_distances = np.zeros(nr_tr*nr_tst+1)
+    edit_distances = np.zeros(nr_tr*nr_tst)
     n_id = 0
+    print ("Computing distances")
     for tr_ind,tst_ind in itertools.product(list(range(nr_tr)), list(range(nr_tst))):
         edit_distances[n_id] = edit_dist(dec_tr_seq[tr_ind,:],dec_tst_seq[tst_ind,:])
         n_id=n_id+1
