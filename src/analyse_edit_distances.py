ac12ac32557382b62de999190c45472e61c03099
Karol Jurasiński <k.jurasinski@gmail.com>
2018-06-17 22:44:36 +0200

    paralelized

--- src/analyse_edit_distances.py
+++ src/analyse_edit_distances.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 import sys
 import os
 import pickle
+from multiprocessing.pool import ThreadPool
 #First analyze the distance to a set of testing sets
 #sequences = dna.load_dna_data(100,1000,"../Data",["Human"],"pos")
 #seq_subset = sequences[0][0]
@@ -30,25 +31,45 @@ outputName=sys.argv[2]
 epfiles = ['../TripleGAN_dna_50_200/'+filename for filename in os.listdir('../TripleGAN_dna_50_200/') if filename.startswith(seq_dir)]
 epfiles=np.sort(epfiles)
 print(epfiles)
+dists={}
+def perform_the_thing(filename):
+    with open(filename, "r") as f:
+        seq_array_local = []
+        # decode seq to strings
+        for line in f:
+            # Strip the endline character from each input line
+            line = line.rstrip("\n")
+            seq_array_local.append(list(line))
+        print('Analysing ' + filename)
+        seq_array_local = seq_array_local[0:50]
+    dists[filename] = evaluateSequences.pairwise_edit_distances(np.asarray(seq_array_local),np.asarray(seq_array_local), False)
+    print("%s finished" % filename)
 
 #compute distances between sequences every 10 epochs
 #epochs = ['0'+str(i)+'0' for i in range(0,10)]
 #epochs.append('100')
 #epochs.append('109')
 #dec_seq_subset2 = evaluateSequences.decode_dna_matrices(seq_subset2)
-dists=[]
-for filename in epfiles:
-#    filename = +'_'+ep+'.txt'
-    fileInput = open(filename, "r")
-    seq_array=[]
-#decode seq to strings
-    for strLine in fileInput:
-        #Strip the endline character from each input line
-        strLine = strLine.rstrip("\n")
-        seq_array.append(list(strLine))
-    print('Analysing '+filename)   
-    seq_array = seq_array[0:50]
-    dists.append(evaluateSequences.pairwise_edit_distances (np.asarray(seq_array),np.asarray(seq_array),False))
+
+# for filename in epfiles:
+# #    filename = +'_'+ep+'.txt'
+#     fileInput = open(filename, "r")
+#     seq_array=[]
+# #decode seq to strings
+#     for strLine in fileInput:
+#         #Strip the endline character from each input line
+#         strLine = strLine.rstrip("\n")
+#         seq_array.append(list(strLine))
+#     print('Analysing '+filename)
+#     seq_array = seq_array[0:50]
+#     dists.append(evaluateSequences.pairwise_edit_distances (np.asarray(seq_array),np.asarray(seq_array),False))
+
+pool = ThreadPool(processes=5)
+pool.map(perform_the_thing, [filename for filename in epfiles])
+pool.close()
+pool.join()
+
+print("Done with concurrent for loop")
 
 #save distances
 output_file = outputName+".txt"
